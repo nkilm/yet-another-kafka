ec665ff0ecffe3561ed76a49c0a8e7c1a25cbb25
Nikhil Mohite <nikhilmohitelhs@gmail.com>
2022-11-23 19:22:13 +0530

    add skeleton code for producer and consumer

--- yak/consumer.py
+++ yak/consumer.py
@@ -7,3 +7,33 @@
 
 
 """
+
+
+class Consumer:
+    """YAK class to consume messages published to a topic"""
+
+    count = 0
+
+    def __init__(self) -> None:
+        Consumer.count += 1
+
+    def __del__(self):
+        """
+        Destructor for YAK class Consumer.
+        E.g:
+        c = Consumer()
+
+        del c # this will call destructor
+        """
+        Consumer.count -= 1
+
+    @classmethod
+    def get_producer_count():
+        return Consumer.count
+
+    def recv(topic: str, from_beginning: bool = False) -> str:
+        """
+        Consume message(s) from the topic.
+        if from_beginning=True then all the messages which were published to the topic will be returned
+        """
+        pass
--- yak/producer.py
+++ yak/producer.py
@@ -5,3 +5,37 @@
 - Number of producers should be dynamic
 
 """
+
+
+class Producer:
+    """YAK class to publish messages to particular topic"""
+
+    count = 0
+
+    def __init__(self) -> None:
+        Producer.count += 1
+
+    def __del__(self):
+        """
+        Destructor for YAK class Producer.
+
+        E.g:
+        p = Producer()
+
+        del p # this will call destructor
+        """
+        Producer.count -= 1
+
+    @classmethod
+    def get_producer_count():
+        return Producer.count
+
+    def send(topic: str, msg: str, from_beginning: bool = False):
+        """
+        Publish message(s) to the topic.
+        if from_beginning=True then all the messages which were published to the topic will be returned
+        """
+        pass
+
+    def clean_topic(topic: str):
+        pass
